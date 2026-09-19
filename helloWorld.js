c25bd4728688bd2bd4c966bd36975d3e2aac06c6
null <kailalujan@westernwyoming.edu>
2026-09-19 00:46:08 +0000

    added variables and printed to terminal

--- helloWorld.js
+++ helloWorld.js
@@ -3,3 +3,16 @@ helloWorld.js
 9/15/2026 */
 
 let hello = "Hello World";
+console.log(typeof hello);
+
+let greeting = "Hi, the program is running successfully!";
+let decimalNumber = 4.5;
+let zero = 0;
+let negativeNumber = -10;
+let isTrue = true;
+
+console.log(typeof greeting)
+console.log(typeof decimalNumber)
+console.log(typeof zero)
+console.log(typeof negativeNumber)
+console.log(typeof isTrue)
